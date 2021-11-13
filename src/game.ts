0ca94acab731d145456a0994a0ea5d637eb35e9f
pedroalles <pedroalles@gmail.com>
2021-11-13 16:24:57 -0300

    feat: all ones game

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,12 @@
 export class Game {
+    private _score: number = 0;
+
+    get score(): number {
+        return this._score;
+    }
+
     roll(pins: number): void {
-
+        this._score += pins;
     }
 
-    score(): number {
-        return 0;
-    }
 }
