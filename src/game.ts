2f0a23125ce13775fcc8ba5e42d07cd4b2cf9c19
pedroalles <pedroalles@gmail.com>
2021-11-13 17:31:26 -0300

    feat: spare score

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,5 @@
+import { roots } from "../jest.config";
+
 export class Game {
     private rolls: number[] = Array(21).fill(0);
     private currentRoll: number = 0;
@@ -6,9 +8,20 @@ export class Game {
     score(): number {
         let score = 0;
         let i = 0;
+
         for (let frame = 0; frame < 10; frame += 1) {
-            score += this.rolls[i] + this.rolls[i + 1];
-            i += 2;
+
+            //spare case
+            if (this.rolls[i] + this.rolls[i + 1] === 10) {
+                score += 10 + this.rolls[i + 2];
+                i += 2;
+            }
+            //normal case
+            else {
+                score += this.rolls[i] + this.rolls[i + 1];
+                i += 2;
+            }
+
         }
         return score;
     }
