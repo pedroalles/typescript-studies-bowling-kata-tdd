f5f87e320175b5c65cf5a3493b0612e4da89b7b2
pedroalles <pedroalles@gmail.com>
2021-11-13 18:06:04 -0300

    refactor: rename score method variable

--- src/game.ts
+++ src/game.ts
@@ -1,25 +1,22 @@
-import { roots } from "../jest.config";
-
 export class Game {
     private rolls: number[] = Array(21).fill(0);
     private currentRoll: number = 0;
 
-
     score(): number {
         let score = 0;
-        let i = 0;
+        let frameIndex = 0;
 
         for (let frame = 0; frame < 10; frame += 1) {
 
             //spare case
-            if (this.rolls[i] + this.rolls[i + 1] === 10) {
-                score += 10 + this.rolls[i + 2];
-                i += 2;
+            if (this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10) {
+                score += 10 + this.rolls[frameIndex + 2];
+                frameIndex += 2;
             }
             //normal case
             else {
-                score += this.rolls[i] + this.rolls[i + 1];
-                i += 2;
+                score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+                frameIndex += 2;
             }
 
         }
@@ -30,5 +27,4 @@ export class Game {
         this.rolls[this.currentRoll] = pins;
         this.currentRoll += 1;
     }
-
 }
